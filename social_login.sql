192e157c00719c5602d33044eb6beee3a1fd5967
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2021-11-30 21:46:45 -0500

    Delete social_login.sql

--- social_login.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM cuenta_user;
-
-INSERT INTO cuenta_user (nombre, apellido, usuario, contrasena, email) VALUES ('Christian', 'Jimbo', 'cfjimbo', 'hola1234', 'cfjimbo@gmail,com');
-INSERT INTO cuenta_user (nombre, apellido, usuario, contrasena, email) VALUES ('Juan', 'Cevallos', 'jcevallos', 'adios1234', 'jcevallos@gmail.com');
