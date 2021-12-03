7a4d3bc72e8a086b2662dbebed594189a72ae4b8
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2021-12-02 21:16:04 -0500

    Update social_login.sql

--- social_login.sql
+++ social_login.sql
@@ -1,13 +1,3 @@
-CREATE DATABASE `social_login`;
-
-CREATE TABLE estudiante(
-idcuenta int primary key not null,
-nombre varchar (25),
-apellido varchar (25),
-usuario varchar (25),
-contrasena varchar (25),
-email varchar (25));
-
 SELECT * FROM estudiante;
 
 INSERT INTO estudiante (nombre, apellido, usuario, contrasena, email) VALUES ('Christian', 'Jimbo', 'cfjimbo', 'hola1234', 'cfjimbo@gmail,com');
